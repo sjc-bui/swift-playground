5f0dc9423012cb8aa58c52855f06f4d998a33ba7
sjc-bui <bui@setjapan.co.jp>
2021-06-20 15:04:40 +0900

    Protocols.

--- Sources/Swift.playground/Pages/Extensions.xcplaygroundpage/Contents.swift
+++ Sources/Swift.playground/Pages/Extensions.xcplaygroundpage/Contents.swift
@@ -8,10 +8,36 @@ extension Int {
       completion(i)
     }
   }
+
+  /** Calculate square of it self.*/
+  mutating func square() {
+    self = self * self
+  }
+
+  enum Kind {
+    case negative, zero, positive
+  }
+
+  var kind: Kind {
+    switch self {
+      case 0:
+        return .zero
+      case let x where x > 0:
+        return .positive
+      default:
+        return .negative
+    }
+  }
 }
 
 3.repetitions { i in
   print("Done - \(i)")
 }
+
+var num = 5
+num.square()
+print(num)
+
+print(0.kind)
 
 //: [Next](@next)
